3364da2f7779a3bf4aa3c5b60c07c8da42a45c0d
deadlium <uddeshjais.21@gmail.com>
2024-04-22 03:44:14 +0530

    mui intigration [done]

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,35 @@
+// import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
+// import Head from 'next/head';
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <AppCacheProvider {...props}>
+//       <Head>
+//         <Component {...pageProps} />
+//       </Head>
+//     </AppCacheProvider>
+//   );
+// }
+
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <AppCacheProvider {...props}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </AppCacheProvider>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/v14-pagesRouter';
 
-export default function Document() {
+export default function MyDocument(props) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +21,8 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
