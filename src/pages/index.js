84cd8da7bbbd02c252f2ff062d901672bd5375e5
deadlium <uddeshjais.21@gmail.com>
2024-08-24 14:24:30 +0530

    google analytic add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,26 @@
+import Head from 'next/head'
 import React from 'react'
 
 export default function Index() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-EYZRXJNM1Q`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EYZRXJNM1Q',{
+            page_path: window.location.pathname
+          });
+          `
+          }} />
+      </Head>
+      <main>
+        Home
+      </main>
+    </>
   )
 }
